f3649afdaa9c16becb1ad34636d2346bafce10be
Bogost <bogumilostapiuk@gmail.com>
2019-04-01 22:27:01 +0200

    test_v1: test nr1

---
+++ src/test/java/lab2_1/BinarySearchTest.java
@@ -0,0 +1,28 @@
+package lab2_1;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import edu.iis.mto.bsearch.BinarySearch;
+import edu.iis.mto.bsearch.SearchResult;
+
+public class BinarySearchTest {
+
+    int[] oneElement;
+
+    @Before
+    public void setup() {
+        int[] oneElementInitializer = {5};
+        oneElement = oneElementInitializer;
+    }
+
+    @Test
+    public void elementIsInSequence() {
+        SearchResult searchResult = BinarySearch.search(5, oneElement);
+        assertEquals(searchResult.isFound(), true);
+        assertEquals(searchResult.getPosition(), 1);
+    }
+
+}
