9e5e2b40264dba1b067b3ebbc99d374c24a18e6b
Bogost <bogumilostapiuk@gmail.com>
2019-04-01 22:36:17 +0200

    testy_v1: test nr3

--- src/test/java/lab2_1/BinarySearchTest.java
+++ src/test/java/lab2_1/BinarySearchTest.java
@@ -11,11 +11,14 @@ import edu.iis.mto.bsearch.SearchResult;
 public class BinarySearchTest {
 
     int[] oneElement;
+    int[] manyElements;
 
     @Before
     public void setup() {
         int[] oneElementInitializer = {5};
         oneElement = oneElementInitializer;
+        int[] manyElementsInitializer = {2, 5, 6, 9, 3};
+        manyElements = manyElementsInitializer;
     }
 
     @Test
@@ -29,5 +32,12 @@ public class BinarySearchTest {
     public void elementIsNotInSequence() {
         SearchResult searchResult = BinarySearch.search(4, oneElement);
         assertEquals(searchResult.isFound(), false);
+    }
+
+    @Test
+    public void elementIsFirstInSequence() {
+        SearchResult searchResult = BinarySearch.search(2, manyElements);
+        assertEquals(searchResult.isFound(), true);
+        assertEquals(searchResult.getPosition(), 1);
     }
 }
